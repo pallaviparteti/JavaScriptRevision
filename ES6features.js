2ad9a70af8885db9bb317c76eb6a51c60d639ee8
pallaviparteti <pallaviparteti2000@gmail.com>
2023-09-13 18:13:09 +0530

    [pallavi] Added :implementation  promise of ES6 features

--- ES6features.js
+++ ES6features.js
@@ -232,3 +232,34 @@ console.log(array2);
 // [ 'a', 'b', 'c', 'd' ]
 console.log(array);
 // [ 'a', 'b', 'c' ]
+
+//promise
+
+const promise = new Promise((resolve , reject)=>{
+    let number = 20;
+    setTimeout(()=>{
+        if(number%10 == 0){
+        resolve("number divisible by 10")
+    }
+    else{
+        reject("number is not divisible by 10")
+    }
+    },3000)
+    
+})
+
+function print(){
+    // console.log( await promise)
+    // // return promise.then((message)=>{
+    // //     console.log(message);
+    // // }).catch((error)=>{
+    // //     console.log(error);
+    // // })
+    return promise
+}
+
+async function returnPromise(){
+    const value = await print()
+    console.log(value)
+}
+returnPromise()
